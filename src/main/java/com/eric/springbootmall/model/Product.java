c2e3da1ca424ce04af5f938186f7d9ceb4260d41
eric <26416387.re@gmail.com>
2023-11-19 01:17:08 +0800

    商品功能 - 查詢功能

---
+++ src/main/java/com/eric/springbootmall/model/Product.java
@@ -0,0 +1,20 @@
+package com.eric.springbootmall.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Product {
+
+    private Integer productId;
+    private String productName;
+    private String category;
+    private String imageUrl;
+    private Integer price;
+    private Integer stock;
+    private String description;
+    private java.util.Date createdDate;
+    private Date lastModifiedDate;
+
+}
