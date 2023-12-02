2e4e89ac3c94b8df3b4d1d845d7c7afd0cc3e963
eric <26416387.re@gmail.com>
2023-12-02 17:00:50 +0800

    帳號功能 - 登入加密

--- src/main/java/com/eric/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/eric/springbootmall/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -36,24 +37,33 @@ public class UserServiceImpl implements UserService {
             log.warn(errorMessage);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,errorMessage);
         }
+        //使用MD5加密
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
 
+        //創建帳號
         return userDao.creatUser(userRegisterRequest);
     }
 
     @Override
     public User login(UserloginRequest userloginRequest) {
         User user = userDao.getUserByEmail(userloginRequest.getEmail());
-
+        //檢查user是否存在
         if(user == null){
             log.warn("該 email {} 並未註冊",userloginRequest.getEmail());
             throw  new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if(user.getPassword().equals(userloginRequest.getPassword())){
+        String hashedPassword = DigestUtils.md5DigestAsHex(userloginRequest.getPassword().getBytes());
+
+        //比較密碼
+        if(user.getPassword().equals(hashedPassword)){
             return user;
         }else {
             log.warn("email密碼不正確",userloginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
     }
+
+
 }
